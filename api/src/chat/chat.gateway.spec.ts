f11135e58b245132f8ae1b8b280de17f2a6f0cb7
Gabriel Victor <gabrielvictorweb@outlook.com>
2024-05-14 23:34:37 -0300

    feat: websocket

---
+++ api/src/chat/chat.gateway.spec.ts
@@ -0,0 +1,54 @@
+import { Test } from '@nestjs/testing';
+import { ChatGateway } from './chat.gateway';
+import { INestApplication } from '@nestjs/common';
+import { Socket, io } from 'socket.io-client';
+
+async function createNestApp(...gateways: any): Promise<INestApplication> {
+  const testingModule = await Test.createTestingModule({
+    providers: gateways,
+  }).compile();
+  return testingModule.createNestApplication();
+}
+
+describe('ChatGateway', () => {
+  let gateway: ChatGateway;
+  let app: INestApplication;
+  let ioClient: Socket;
+
+  beforeAll(async () => {
+    // Instantiate the app
+    app = await createNestApp(ChatGateway);
+    // Get the gateway instance from the app instance
+    gateway = app.get<ChatGateway>(ChatGateway);
+    // Create a new client that will interact with the gateway
+    ioClient = io('http://localhost:3000', {
+      autoConnect: false,
+      transports: ['websocket', 'polling'],
+    });
+
+    app.listen(3000);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be defined', () => {
+    expect(gateway).toBeDefined();
+  });
+
+  it('should emit "pong" on "ping"', async () => {
+    ioClient.connect();
+    ioClient.emit('ping', 'Hello world!');
+    await new Promise<void>((resolve) => {
+      ioClient.on('connect', () => {
+        console.log('connected');
+      });
+      ioClient.on('pong', (data) => {
+        expect(data).toBe('Hello world!');
+        resolve();
+      });
+    });
+    ioClient.disconnect();
+  });
+});
