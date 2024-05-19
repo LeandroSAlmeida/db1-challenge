40377949f16274a25e8becfde940300e0b29970e
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-05-19 07:51:08 -0300

    feat: tests archive

---
+++ gui/app/src/main/java/com/challenge/db1/sampledata/SampleData.kt
@@ -0,0 +1,34 @@
+package com.challenge.db1.sampledata
+
+import com.challenge.db1.domain.AlunoEProfessor
+
+val SampleAlunos = listOf(
+
+
+        AlunoEProfessor(
+            name = "Leandro",
+            null,
+            true,
+            "Java",
+            "Programar",
+            "Fiap"
+            ),
+            AlunoEProfessor(
+                name = "teste",
+                null,
+                true,
+                "Java",
+                "Programar",
+                "Fiap"
+            ),
+
+            AlunoEProfessor(
+                name = "teste",
+                null,
+                true,
+                "Java",
+                "Programar",
+                "Fiap"
+            )
+
+)
