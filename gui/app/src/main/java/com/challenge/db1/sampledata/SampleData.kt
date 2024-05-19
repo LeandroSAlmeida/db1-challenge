6c6e8774bdebc0cc6c0319527fec41fff5429353
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-05-19 11:04:49 -0300

    refactor: change sampledata for more complex tests

--- gui/app/src/main/java/com/challenge/db1/sampledata/SampleData.kt
+++ gui/app/src/main/java/com/challenge/db1/sampledata/SampleData.kt
@@ -3,32 +3,63 @@ package com.challenge.db1.sampledata
 import com.challenge.db1.domain.AlunoEProfessor
 
 val SampleAlunos = listOf(
+    AlunoEProfessor(
+        name = "Aluno1",
+        null,
+        true,
+        "Java",
+        "Programar",
+        "Fiap"
+    ),
+    AlunoEProfessor(
+        name = "Aluno2",
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/461.jpg",
+        true,
+        "Java",
+        "Programar",
+        "Fiap"
+    ),
 
+    AlunoEProfessor(
+        name = "Aluno3",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Faluno_3413565&psig=AOvVaw2lniMx19rB_-Po4s_avwdB&ust=1716207081949000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCqn_jXmYYDFQAAAAAdAAAAABAD",
+        true,
+        "Java",
+        "Programar",
+        "Fiap"
+    )
 
-        AlunoEProfessor(
-            name = "Leandro",
-            null,
-            true,
-            "Java",
-            "Programar",
-            "Fiap"
-            ),
-            AlunoEProfessor(
-                name = "teste",
-                null,
-                true,
-                "Java",
-                "Programar",
-                "Fiap"
-            ),
+)
 
-            AlunoEProfessor(
-                name = "teste",
-                null,
-                true,
-                "Java",
-                "Programar",
-                "Fiap"
-            )
+val SampleProfessor = listOf(
+    AlunoEProfessor(
+        name = "TesteProfessor",
+        null,
+        true,
+        "Java",
+        "Programar",
+        "Fiap"
+    ),
+    AlunoEProfessor(
+        name = "TesteProfessor2",
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/461.jpg",
+        true,
+        "TypeScript",
+        "Programar",
+        "Fiap"
+    ),
 
+    AlunoEProfessor(
+        name = "TesteProfessor3",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Faluno_3413565&psig=AOvVaw2lniMx19rB_-Po4s_avwdB&ust=1716207081949000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCqn_jXmYYDFQAAAAAdAAAAABAD",
+        true,
+        "React",
+        "Programar",
+        "Fiap"
+    )
+)
+
+val SampleSections = mapOf (
+    "alunos" to SampleAlunos,
+    "professor" to SampleProfessor
 )
